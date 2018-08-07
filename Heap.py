baa487431ad75b459c45436f34311ae1b12c1cc7
iob764rv <iic287m@gmail.com>
2018-08-07 19:02:27 -0400

    Update Heap.py

--- Heap.py
+++ Heap.py
@@ -1,10 +1,14 @@
 class Heap:
   from Node import Node
   
+  global nodeBar
   nodeBar = Node()
   
   def addValue(a):
+    from Node import Node
     #add a value to the heap
+    
     node=Node().setInput(a)
-    if nodeBar.value== None:
+    if nodeBar.value== None: #condition for empty tree
+      
       nodeBar.setValue(a)

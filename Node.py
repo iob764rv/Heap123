dbbc06b8a9ac87ae5c20bdf55739b8c94f8e4ba5
iob764rv <iic287m@gmail.com>
2018-08-15 22:04:28 -0400

    Update Node.py

--- Node.py
+++ Node.py
@@ -1,14 +1,11 @@
 class Node:
   #A class of Node is used in heap
-  leftNode=int;
-  rightNode=int;
-  value=None;
   
   def __init__(self):
     #init
     self.leftNode= None
     self.rightNode= None
-    self.value =None
+    self.value =None #use as the value of the node
   def setValue(newValue):
     self.value=newValue
     
